285b3748097c32e6423d2d367968a6f02fe2af27
eunseo76 <eunseolee19@gmail.com>
2026-07-14 09:41:00 +0900

    fix: 금액 10원 이하 단위 잘리는 문제 해결

--- lib/core/utils/number_format.dart
+++ lib/core/utils/number_format.dart
@@ -6,7 +6,7 @@ String formatNumber(double value) {
 
 String formatManwon(double? value) {
   if (value == null || value == 0) return '-';
-  return NumberFormat('#,##0.##').format(value / 10000);
+  return NumberFormat('#,##0.####').format(value / 10000);
 }
 
 String formatCompact(double value) {
